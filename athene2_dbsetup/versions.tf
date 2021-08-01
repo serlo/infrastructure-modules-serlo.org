67c4a96640d68b340e69326d3fa4c06f3bc1a49e
Jonas Keinholz <git@inyono.dev>
2021-08-01 14:59:12 +0200

    terraform v1.0

--- athene2_dbsetup/versions.tf
+++ athene2_dbsetup/versions.tf
@@ -1,3 +1,13 @@
 terraform {
-  required_version = ">= 0.15"
+  required_version = ">= 1.0"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.0"
+    }
+  }
 }
